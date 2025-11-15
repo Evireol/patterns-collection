35cc19a4e9882f0c03ac4f1d463e57750e909e14
Katerina Kuznetsova <izorog@gmail.com>
2025-11-15 23:58:48 +0300

    Общий файл обращений к примерам паттернов

---
+++ index.php
@@ -0,0 +1,27 @@
+// DESIGNPATTERNSEXAMPLES/index.php (корневой)
+<!DOCTYPE html>
+<html>
+<head><title>Design Patterns Examples</title></head>
+<body>
+<h1>Паттерны проектирования</h1>
+<ul>
+    <li><a href="Adapter/">Adapter</a></li>
+    <li><a href="Bridge/">Bridge</a></li>
+    <li><a href="Builder/">Builder</a></li>
+    <li><a href="Chain of Responsibility/">Chain of Responsibility</a></li>
+    <li><a href="Decorator/">Decorator</a></li>
+    <li><a href="Facade/">Facade</a></li>
+    <li><a href="Front Controller/">Front Controller</a></li>
+    <li><a href="Iterator/">Iterator</a></li>
+    <li><a href="Multiton/">Multiton (антипаттерн)</a></li>
+    <li><a href="null object/">null object</a></li>
+    <li><a href="Observer/">Observer</a></li>
+    <li><a href="Prototype/">Prototype</a></li>
+    <li><a href="State/">State</a></li>
+    <li><a href="Strategy/">Strategy</a></li>
+    <li><a href="Template Method/">Template Method</a></li>
+    <li><a href="Компоновщик/">Компоновщик</a></li>
+    <!-- и т.д. -->
+</ul>
+</body>
+</html>
